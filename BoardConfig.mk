6c56e8eea91c300ffb88b91829e7d2b26117de5e
Jarl-Penguin <jarlpenguin@outlook.com>
2020-05-13 10:18:50 +0300

    montana: Setup NFC
    Signed-off-by: Jarl-Penguin <jarlpenguin@outlook.com>
    Change-Id: Ibc12ecd0615732b420bd105a57343f98d013ddac

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -12,6 +12,9 @@ DEVICE_PATH := device/motorola/montana
 # Assert
 TARGET_OTA_ASSERT_DEVICE := montana
 
+# HIDL
+DEVICE_MANIFEST_FILE += $(DEVICE_PATH)/manifest.xml
+
 # Kernel
 TARGET_KERNEL_CONFIG := montana_defconfig
 
